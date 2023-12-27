cf0fb9c76325b84466a316bee0a208b833c330bf
Dan Rosu <cristian.rosu@student.upt.ro>
2023-12-27 16:09:59 +0200

    first-commit

---
+++ src/main/java/com/meetmountain/meetmountainapp/MeetmountainAppApplication.java
@@ -0,0 +1,13 @@
+package com.meetmountain.meetmountainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetmountainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetmountainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetmountain/meetmountainapp/MeetmountainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetmountain.meetmountainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetmountainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
