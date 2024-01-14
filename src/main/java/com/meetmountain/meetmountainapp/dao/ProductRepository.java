8a02d8a073a27b216ae8fa9eac529499cc7f7d9d
Dan Rosu <cristian.rosu@student.upt.ro>
2024-01-14 23:19:35 +0200

    add a new search method

--- src/main/java/com/meetmountain/meetmountainapp/dao/ProductRepository.java
+++ src/main/java/com/meetmountain/meetmountainapp/dao/ProductRepository.java
@@ -13,4 +13,6 @@ import javax.swing.*;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
